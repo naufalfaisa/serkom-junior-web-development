8d3643b0901e606d289f89b578e3b4131d068b89
naufalfaisa <naufalfaisa386@gmail.com>
2025-11-15 09:53:43 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,40 @@
+// Tunggu sampai halaman selesai dimuat
 $(document).ready(function() {
-    const $gambar = $('#gambar');
-    const $tombolGambar = $('#ubahGambarBtn');
-    const $tombolTema = $('#toggleThemeBtn');
-    const $body = $('body');
-    const $cards = $('.card');
-    const gambarList = [
-        // List gambar
-        'assets/media/gambar1.jpg',
-        'assets/media/gambar2.jpg'
-    ];
-    // Posisi awal gambar
-    let index = 0;
 
-    // Ubah gambar
-    function ubahGambar() {
-        index = (index + 1) % gambarList.length;
-        $gambar.attr('src', gambarList[index]);
-        console.log('Gambar diubah ke:', gambarList[index]);
+    const gambar = $('#gambar');                // gambar di card
+    const tombolGambar = $('#ubahGambarBtn');   // tombol untuk ganti gambar
+    const tombolTema = $('#toggleThemeBtn');    // tombol untuk ganti tema
+    const body = $('body');                     // seluruh body halaman
+    const cards = $('.card');                   // semua card
+
+    // Daftar gambar yang bisa diganti
+    const daftarGambar = [
+        'assets/media/gambar1.jpg',
+        'assets/media/gambar2.jpg',
+        'assets/media/gambar3.jpg'
+    ];
+
+    // Indeks gambar saat ini
+    let indeksGambar = 0;
+
+    // Fungsi untuk ganti gambar
+    function gantiGambar() {
+        indeksGambar++;  // tambah 1
+        if (indeksGambar >= daftarGambar.length) {
+            indeksGambar = 0;  // kembali ke gambar pertama jika sudah akhir
+        }
+        gambar.attr('src', daftarGambar[indeksGambar]); // ubah src gambar
+        console.log('Gambar sekarang:', daftarGambar[indeksGambar]);
     }
 
-    // Ganti Tema
-    function toggleTema() {
-        $body.toggleClass('bg-black text-light');
-        $cards.toggleClass('bg-dark text-light');
+    // Fungsi untuk ganti tema
+    function gantiTema() {
+        body.toggleClass('bg-black text-light');  // ubah background dan teks body
+        cards.toggleClass('bg-dark text-light');  // ubah background dan teks card
         console.log('Tema halaman diganti');
     }
 
-    $tombolGambar.on('click', ubahGambar);
-    $tombolTema.on('click', toggleTema);
+    // Pasang event klik ke tombol
+    tombolGambar.on('click', gantiGambar);
+    tombolTema.on('click', gantiTema);
 });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const gambar = document.getElementById('gambar');
-//     const tombolGambar = document.getElementById('ubahGambarBtn');
-//     const tombolTema = document.getElementById('toggleThemeBtn');
-//     const body = document.body;
-//     const cards = document.querySelectorAll('.card');
-
-//     const gambarList = [
-//         'assets/media/gambar1.jpg',
-//         'assets/media/gambar2.jpg'
-//     ];
-
-//     let index = 0;
-
-//     // Ubah gambar
-//     function ubahGambar() {
-//         index = (index + 1) % gambarList.length;
-//         gambar.src = gambarList[index];
-//         console.log('Gambar diubah ke:', gambarList[index]);
-//     }
-
-//     // Ganti Tema
-//     function toggleTema() {
-//         body.classList.toggle('bg-black');
-//         body.classList.toggle('text-light');
-
-//         cards.forEach(card => {
-//             card.classList.toggle('bg-dark');
-//             card.classList.toggle('text-light');
-//         });
-
-//         console.log('Tema halaman diganti');
-//     }
-
-//     tombolGambar.addEventListener('click', ubahGambar);
-//     tombolTema.addEventListener('click', toggleTema);
-// });
